529836a963dbfaa0329dabc0386634954d2d31c9
xj962458 <2291921855@qq.com>
2020-01-22 16:22:31 +0800

    xj-java

---
+++ xj/src/xj/程序/xj2.java
@@ -0,0 +1,10 @@
+public class xj2{
+    public static void main(final String[] args){
+        int i,j;
+        for(i=1;i<=9;i++)
+        {
+            for(j=i;j<=9;j++)
+                System.out.println(i*j);
+        }
+    }
+}
